18888f801e0a55970c7e23a533a15e06ad537966
thanhxuan8901 <88064168+thanhxuan8901@users.noreply.github.com>
2021-08-10 13:11:04 +0700

    Update setups.js

--- handlers/setups.js
+++ handlers/setups.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
      client.on("ready", async() => {
    setInterval(() => {
       stateswitch = !stateswitch; //thay đổi trạng thái
-         if (stateswitch) client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+         if (stateswitch) client.user.setActivity(`${default_prefix}help and ${default_prefix}play`, { type: "LISTENING" });
          else client.user.setActivity(`${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} Thành viên | ${client.guilds.cache.size} Server `, { type: "PLAYING" });
           
    }, 2000); 
